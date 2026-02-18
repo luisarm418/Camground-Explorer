99b092d6a4faef787b347657ad9951ba36f16643
snowfrost12 <luisarm418@outlook.com>
2026-02-18 18:05:15 -0500

    updated

--- app/src/main/java/com/codepath/campgrounds/MainActivity.kt
+++ app/src/main/java/com/codepath/campgrounds/MainActivity.kt
@@ -21,6 +21,7 @@ fun createJson() = Json {
 
 private const val TAG = "CampgroundsMain/"
 private val PARKS_API_KEY = BuildConfig.API_KEY
+
 private val CAMPGROUNDS_URL =
     "https://developer.nps.gov/api/v1/campgrounds?api_key=${PARKS_API_KEY}"
 
@@ -29,7 +30,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     // TODO: Create campgrounds list
-
+    private val campgrounds = mutableListOf<Campground>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -40,7 +41,8 @@ class MainActivity : AppCompatActivity() {
         campgroundsRecyclerView = findViewById(R.id.campgrounds)
 
         // TODO: Set up CampgroundAdapter with campgrounds
-
+        val campgroundAdapter = CampgroundAdapter(this, campgrounds)
+        campgroundsRecyclerView.adapter = campgroundAdapter
 
         campgroundsRecyclerView.layoutManager = LinearLayoutManager(this).also {
             val dividerItemDecoration = DividerItemDecoration(this, it.orientation)
@@ -62,11 +64,16 @@ class MainActivity : AppCompatActivity() {
                 Log.i(TAG, "Successfully fetched campgrounds: $json")
                 try {
                     // TODO: Create the parsedJSON
-
+                    val parsedJson = createJson().decodeFromString(
+                        CampgroundResponse.serializer(),
+                        json.jsonObject.toString()
+                    )
                     // TODO: Do something with the returned json (contains campground information)
-
-                    // TODO: Save the campgrounds and reload the screen
-
+                    parsedJson.data?.let { list ->
+                        campgrounds.addAll(list)
+                        // TODO: Notify the adapter that the dataset has changed
+                        campgroundAdapter.notifyDataSetChanged()
+                    }
                 } catch (e: JSONException) {
                     Log.e(TAG, "Exception: $e")
                 }
